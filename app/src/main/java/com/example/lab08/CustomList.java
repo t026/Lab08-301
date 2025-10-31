e2671e0c84dadfcce84d9edc9657a39485f6fea4
t026 <tuhintanishq@gmail.com>
2025-10-30 21:44:20 -0600

    Implemented  CustomList.deleteCity(), and failing testCountCity()

--- app/src/main/java/com/example/lab08/CustomList.java
+++ app/src/main/java/com/example/lab08/CustomList.java
@@ -2,6 +2,7 @@ package com.example.lab08;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.NoSuchElementException;
 
 public class CustomList {
     private List<City> cities;
@@ -19,5 +20,15 @@ public class CustomList {
     // Will be implemented later using TDD (leave blank for now)
     public boolean hasCity(City city) {
         return cities.contains(city);
+    }
+
+    public void deleteCity(City city){
+        if (hasCity(city)) {
+            cities.remove(city);
+        } else {
+            throw new NoSuchElementException(
+                    String.format("No city named **%s** in list", city.getCityName())
+            );
+        }
     }
 }
--- app/src/test/java/com/example/lab08/CustomListTest.java
+++ app/src/test/java/com/example/lab08/CustomListTest.java
@@ -22,4 +22,21 @@ public class CustomListTest {
         assertFalse(list.hasCity(calgary));
     }
 
+    @Test
+    public void testCountCity(){
+        CustomList list = new CustomList();
+        assertEquals(0, list.countCities());
+        City calgary = new City("Calgary", "AB");
+        list.addCity(calgary);
+        assertEquals(1, list.countCities());
+
+        City edmonton = new City("Edmonton", "AB");
+        list.addCity(edmonton);
+        assertEquals(2, list.countCities());
+
+        list.deleteCity(calgary);
+        assertEquals(1, list.countCities());
+
+    }
+
 }
